a4b47efebac03fcdc1486b1ce736a07d3826effc
Pedro <peuhenry2018@gmail.com>
2024-02-04 20:27:50 -0300

    Integrate NEED  SOCKET

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ from PIL import Image as PILImage, ImageTk, UnidentifiedImageError
 from io import BytesIO
 from scraping import obter_caminhos_imagens, realizar_scraping_e_baixar_imagens
 
+def limpar_tela(window):
+    global codigoBotao1, codigoBotao2, imagem_selecionada, botaoAcertos
+    window['-IMAGEM1-'].update(data=b'')
+    window['-IMAGEM2-'].update(data=b'')
+    for codigo in range(1, 17):
+        if codigo not in botaoAcertos:
+            window[codigo].update(disabled=False)
+    codigoBotao1 = -1
+    codigoBotao2 = -1
+    imagem_selecionada = None
+
 def criar_layout_selecao_jogadores():
     layout = [
         [Text('Seleção de Jogadores')],
@@ -26,7 +37,7 @@ def obter_endereco_ip_servidor():
 def main_selecao_jogadores():
     exibir_espera_download()
 
-    # Realizar scraping e baixar imagens
+    # Realizar scraping.py e baixar imagens
     realizar_scraping_e_baixar_imagens()
 
     if not os.path.exists('pokemons_img'):
@@ -38,15 +49,14 @@ def main_selecao_jogadores():
 
     try:
         while janela_aberta:
-            event_selecao_jogadores, values_selecao_jogadores = window_selecao_jogadores.read()
+            event_selecao_jogadores, values_selecao_jogadores = window_selecao_jogadores.read(timeout=0)
 
             if event_selecao_jogadores == 'EXIT':
-                janela_aberta = False
                 break
 
-            if values_selecao_jogadores and values_selecao_jogadores['-SERVIDOR-']:
-                window_selecao_jogadores['-ENDERECO-SERVIDOR-'].update(disabled=False, value=obter_endereco_ip_servidor())
-            else:
+            if values_selecao_jogadores['-SERVIDOR-'] and not values_selecao_jogadores['-ENDERECO-SERVIDOR-']:
+                window_selecao_jogadores['-ENDERECO-SERVIDOR-'].update(disabled=True, value=obter_endereco_ip_servidor())
+            elif not values_selecao_jogadores['-SERVIDOR-']:
                 window_selecao_jogadores['-ENDERECO-SERVIDOR-'].update(disabled=True, value='')
 
             if values_selecao_jogadores and values_selecao_jogadores['-CLIENTE-']:
@@ -58,9 +68,12 @@ def main_selecao_jogadores():
                 endereco_ip_servidor = window_selecao_jogadores['-ENDERECO-SERVIDOR-'].get() if values_selecao_jogadores.get('-SERVIDOR-') else None
                 endereco_ip_cliente = window_selecao_jogadores['-ENDERECO-CLIENTE-'].get() if values_selecao_jogadores.get('-CLIENTE-') else None
 
-                janela_aberta = False
-                window_selecao_jogadores.close()
-                main_jogo(endereco_ip_servidor, endereco_ip_cliente)
+                if endereco_ip_servidor and endereco_ip_cliente:
+                    janela_aberta = False
+                    window_selecao_jogadores.close()
+                    main_jogo(endereco_ip_servidor, endereco_ip_cliente)
+                else:
+                    print("Insira os jogadores")
 
     except Exception as e:
         # Tratamento para quando a janela é fechada
@@ -70,9 +83,6 @@ def main_selecao_jogadores():
         window_selecao_jogadores.close()
 
 def criar_layout_jogo(image_paths, endereco_ip_servidor, endereco_ip_cliente):
-    # Seleciona aleatoriamente 8 imagens de Pokémons para formar pares
-    random.shuffle(image_paths)
-    image_paths = image_paths[:8] * 2
 
     layout_grid = [
         [Button(f'{i * 4 + j}', key=i * 4 + j, size=(4, 2), image_filename='', image_size=(100, 100), pad=(5, 5)) for j in range(1, 5)]
@@ -101,7 +111,12 @@ def criar_layout_jogo(image_paths, endereco_ip_servidor, endereco_ip_cliente):
     return layout
 
 def main_jogo(endereco_ip_servidor, endereco_ip_cliente):
+    global codigoBotao1, codigoBotao2, imagem_selecionada, botaoAcertos
     image_paths = obter_caminhos_imagens()
+    # Seleciona aleatoriamente 8 imagens de Pokémons para formar pares
+    random.shuffle(image_paths)
+    image_paths = image_paths[:8] * 2
+    random.shuffle(image_paths)
 
     if not image_paths:
         print("Erro: Não foi possível carregar os caminhos das imagens.")
@@ -117,19 +132,26 @@ def main_jogo(endereco_ip_servidor, endereco_ip_cliente):
     imagem_selecionada = None
     imagem1_key = '-IMAGEM1-'
     imagem2_key = '-IMAGEM2-'
-
+    codigoBotao1 = -1
+    codigoBotao2 = -1
     pontuacao_cliente = 0
     pontuacao_servidor = 0
-
+    jogada = 1
     try:
         while janela_aberta:
             event_jogo, values_jogo = window_jogo.read()
 
             if event_jogo in (None, 'EXIT'):
-                janela_aberta = False
                 break
 
             if event_jogo in range(1, 17):
+                if codigoBotao1 == -1:
+                    codigoBotao1 = event_jogo
+                    window_jogo[event_jogo].update(disabled=True)
+                else:
+                    codigoBotao2 = event_jogo
+                    window_jogo[event_jogo].update(disabled=True)
+
                 if imagem_selecionada is None:
                     imagem_selecionada = event_jogo
                     imagem_path = image_paths[event_jogo - 1]
@@ -142,6 +164,8 @@ def main_jogo(endereco_ip_servidor, endereco_ip_cliente):
                     except (UnidentifiedImageError, IOError) as e:
                         print(f"Erro ao abrir a imagem {imagem_path}: {e}")
                 else:
+                    for codigo in range(1, 17):
+                        window_jogo[codigo].update(disabled=True)
                     imagem_path = image_paths[event_jogo - 1]
                     try:
                         with open(imagem_path, 'rb') as img_file:
@@ -153,23 +177,26 @@ def main_jogo(endereco_ip_servidor, endereco_ip_cliente):
                         print(f"Erro ao abrir a imagem {imagem_path}: {e}")
 
                     if imagem_selecionada is not None and image_paths[event_jogo - 1] == image_paths[imagem_selecionada - 1]:
-                        if is_servidor:
+                        if jogada == 1:
                             pontuacao_servidor += 1
                         else:
                             pontuacao_cliente += 1
 
                         print(f"\nPontuação: Cliente {pontuacao_cliente} - {pontuacao_servidor} Servidor")
-
-                        window_jogo[event_jogo].update(disabled=True)
-                        window_jogo[imagem_selecionada].update(disabled=True)
-
-                        imagem_selecionada = None
+                        botaoAcertos.append(codigoBotao1)
+                        botaoAcertos.append(codigoBotao2)
+                        window_jogo.TKroot.after(1000, lambda: limpar_tela(window_jogo))
 
                     else:
-                        print("\nErrou! As imagens não são iguais.")
-                        window_jogo[imagem1_key].update(data=b'')
-                        window_jogo[imagem2_key].update(data=b'')
-                        imagem_selecionada = None
+                        print("Errou! As imagens não são iguais.")
+                        if jogada == 1:
+                            print("Servidor jogou, vez do cliente")
+                            jogada = 2
+                        else:
+                            print("Cliente jogou, vez do servidor")
+                            jogada = 1
+                        window_jogo.TKroot.after(1000, lambda: limpar_tela(window_jogo))
+
 
     except Exception as e:
         # Tratamento para quando a janela é fechada
@@ -179,4 +206,8 @@ def main_jogo(endereco_ip_servidor, endereco_ip_cliente):
         window_jogo.close()
 
 if __name__ == "__main__":
+    imagem_selecionada = None
+    codigoBotao1 = -1
+    codigoBotao2 = -1
+    botaoAcertos = []
     main_selecao_jogadores()
--- scraping.py
+++ scraping.py
@@ -9,57 +9,58 @@ def baixar_imagens():
     # Crie um diretório para salvar as imagens
     if not os.path.exists('pokemons_img'):
         os.makedirs('pokemons_img')
+        url = 'https://pokemondb.net/pokedex/shiny'
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
 
-    url = 'https://pokemondb.net/pokedex/shiny'
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+        try:
+            resposta = requests.get(url, headers=headers)
+            resposta.raise_for_status()  # Verifica se houve um erro na requisição HTTP
 
-    try:
-        resposta = requests.get(url, headers=headers)
-        resposta.raise_for_status()  # Verifica se houve um erro na requisição HTTP
+            bs = BeautifulSoup(resposta.text, 'html.parser')
 
-        bs = BeautifulSoup(resposta.text, 'html.parser')
+            # Encontrar todos os links <img> com a classe "shinydex-sprite-shiny"
+            imagens = bs.find_all('img', class_='shinydex-sprite-shiny')
 
-        # Encontrar todos os links <img> com a classe "shinydex-sprite-shiny"
-        imagens = bs.find_all('img', class_='shinydex-sprite-shiny')
+            # Lista para armazenar os caminhos das imagens
+            image_paths = []
 
-        # Lista para armazenar os caminhos das imagens
-        image_paths = []
+            # Função para baixar uma imagem e adicionar à lista
+            def baixar_e_adicionar(img_tag):
+                caminho = img_tag['src']
+                img_url = urljoin(url, caminho)
+                local_path = os.path.join('pokemons_img', os.path.basename(img_url))
 
-        # Função para baixar uma imagem e adicionar à lista
-        def baixar_e_adicionar(img_tag):
-            caminho = img_tag['src']
-            img_url = urljoin(url, caminho)
-            local_path = os.path.join('pokemons_img', os.path.basename(img_url))
+                with open(local_path, 'wb') as img_file:
+                    img_data = requests.get(img_url).content
+                    img_file.write(img_data)
 
-            with open(local_path, 'wb') as img_file:
-                img_data = requests.get(img_url).content
-                img_file.write(img_data)
+                # Adicionar o caminho da imagem à lista
+                image_paths.append(local_path)
 
-            # Adicionar o caminho da imagem à lista
-            image_paths.append(local_path)
+            # Usar ThreadPoolExecutor para baixar imagens de forma concorrente
+            with ThreadPoolExecutor() as executor:
+                executor.map(baixar_e_adicionar, imagens)
 
-        # Usar ThreadPoolExecutor para baixar imagens de forma concorrente
-        with ThreadPoolExecutor() as executor:
-            executor.map(baixar_e_adicionar, imagens)
+            # Verificar se foram baixadas imagens
+            if not image_paths:
+                print("Erro: Não foi possível encontrar imagens de Pokémons.")
+                return None
 
-        # Verificar se foram baixadas imagens
-        if not image_paths:
-            print("Erro: Não foi possível encontrar imagens de Pokémons.")
+            # Criar o diretório 'pokemons' se não existir
+            if not os.path.exists('pokemons'):
+                os.makedirs('pokemons')
+
+            # Salvar os caminhos das imagens em um arquivo pickle
+            with open(os.path.join('pokemons', 'image_paths.pkl'), 'wb') as file:
+                pickle.dump(image_paths, file)
+
+            return image_paths
+
+        except requests.exceptions.RequestException as e:
+            print(f"Erro ao acessar o site: {e}")
             return None
 
-        # Criar o diretório 'pokemons' se não existir
-        if not os.path.exists('pokemons'):
-            os.makedirs('pokemons')
-
-        # Salvar os caminhos das imagens em um arquivo pickle
-        with open(os.path.join('pokemons', 'image_paths.pkl'), 'wb') as file:
-            pickle.dump(image_paths, file)
-
-        return image_paths
-
-    except requests.exceptions.RequestException as e:
-        print(f"Erro ao acessar o site: {e}")
-        return None
 
 def obter_caminhos_imagens():
     image_paths_file = os.path.join('pokemons', 'image_paths.pkl')
